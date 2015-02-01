16ef9b35acf69077ef02d57f4e2bc2aedfe5465d
Matthew Hill <matthew.hill4@bskyb.com>
2015-02-01 14:06:33 +0000

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularjsextensionApp
+ * @description
+ * # angularjsextensionApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularJsExtension', []);
