b874c1d820b1add0b15a81bcd3d7e001eefaac6a
matthewjh <matthew.jh@outlook.com>
2015-04-06 22:24:31 +0100

    adds comments

--- content_scripts/scripts/background/model.js
+++ content_scripts/scripts/background/model.js
@@ -1,10 +1,9 @@
-/*
- * The data model for the extension. Holds all the data pertaining to browser tabs.
- */
-
 define([], function () {
   'use strict';
 
+  /**
+   * The data model for the extension. Holds data pertaining to each browser tab that is being used with the extension.
+   */
   return {
 
     /* This is where we store the data for a given tab id. */
