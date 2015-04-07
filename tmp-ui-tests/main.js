ebb5e50155d6874110a0e89349e4df517cb32eea
matthewjh <matthew.jh@outlook.com>
2015-04-07 22:32:10 +0100

    deletes tmp-ui-tests

--- tmp-ui-tests/main.js
+++
@@ -1,9 +0,0 @@
-System.registerModule("ui/main.js", [], function() {
-  "use strict";
-  var __moduleName = "ui/main.js";
-  var $__default = 6;
-  return {get default() {
-      return $__default;
-    }};
-});
-//# sourceURL=ui/main.js
